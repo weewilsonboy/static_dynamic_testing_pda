0c5029e34f25dbd6c6ea5ba8b18b8777ed520948
weewilsonboy <wilson.euan@gmail.com>
2023-11-03 12:22:18 +0000

    all test completed and fixed

--- specs/card_game_tests.py
+++ specs/card_game_tests.py
@@ -2,5 +2,21 @@ import unittest
 from src.card import Card
 from src.card_game import CardGame
 
-class TestCardGame:
-    pass
+
+class TestCardGame(unittest.TestCase):
+    def test_can_check_for_ace(self):
+        card1 = Card("Hearts", 2)
+        card2 = Card("Hearts", 1)
+        self.assertEqual(CardGame.check_for_ace(CardGame, card1), False)
+
+    def test_can_find_highest_card(self):
+        card1 = Card("Hearts", 2)
+        card2 = Card("Hearts", 1)
+        self.assertEqual(CardGame.highest_card(self, card1, card2), card1)
+
+    def test_can_cadd_cards_total(self):
+        card1 = Card("Hearts", 2)
+        card2 = Card("Hearts", 1)
+        self.assertEqual(
+            CardGame.cards_total(self, [card1, card2]), "You have a total of3"
+        )
--- src/card_game.py
+++ src/card_game.py
@@ -4,26 +4,22 @@
 
 # Correct the errors below that you spotted in task 1.
 
+
 class CardGame:
+    def check_for_ace(self, card):
+        if card.value == 1:
+            return True
+        else:
+            return False
 
+    def highest_card(self, card1, card2):
+        if card1.value > card2.value:
+            return card1
+        else:
+            return card2
 
-  def check_for_ace(self, card):
-    if card.value = 1:
-      return True
-    else
-      return False
-   
-
-  dif highest_card(self, card1 card2):
-  if card1.value > card2.value:
-    return card
-  else:
-    return card2
-  
-
-
-def cards_total(self, cards):
-  total
-  for card in cards:
-    total += card.value
-    return "You have a total of" + total
+    def cards_total(self, cards):
+        total = 0
+        for card in cards:
+            total += card.value
+        return f"You have a total of{total}"
