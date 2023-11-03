cb33b3fc79cb49b18f989fc4513a4b187545d98c
weewilsonboy <wilson.euan@gmail.com>
2023-11-03 17:10:43 +0000

    added original card game and comments about errors

---
+++ src/original_card_game.py
@@ -0,0 +1,26 @@
+class CardGame:
+
+
+  def check_for_ace(self, card):
+    if card.value = 1: #should be == to compare, not = to assign
+      return True
+    else #missing a :
+      return False
+   
+
+  dif highest_card(self, card1 card2):#def not dif, each prop within the function 
+                                      #call needs to be separated by a comma
+  if card1.value > card2.value:
+    return card
+  else:
+    return card2
+  
+
+
+def cards_total(self, cards): #indentation is wrong, the whole function
+                              #need to be indented by 1 increment
+  total
+  for card in cards:
+    total += card.value
+    return "You have a total of" + total  #This will return after the first card
+                                          #the return should happen after the loop
