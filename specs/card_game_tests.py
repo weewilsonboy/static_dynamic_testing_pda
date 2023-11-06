bc71ab8d4bfa9735cd02f2214cbb60bab6d53565
weewilsonboy <wilson.euan@gmail.com>
2023-11-06 12:29:49 +0000

    fixed scoping of the card game, and removed self from props to CardGame

--- specs/card_game_tests.py
+++ specs/card_game_tests.py
@@ -4,19 +4,23 @@ from src.card_game import CardGame
 
 
 class TestCardGame(unittest.TestCase):
+    def setUp(self):
+        cardGame = CardGame()
+
     def test_can_check_for_ace(self):
         card1 = Card("Hearts", 2)
         card2 = Card("Hearts", 1)
-        self.assertEqual(CardGame.check_for_ace(CardGame, card1), False)
+        cardGame = CardGame()
+        self.assertEqual(cardGame.check_for_ace(card1), False)
 
     def test_can_find_highest_card(self):
         card1 = Card("Hearts", 2)
         card2 = Card("Hearts", 1)
-        self.assertEqual(CardGame.highest_card(self, card1, card2), card1)
+        cardGame = CardGame()
+        self.assertEqual(cardGame.highest_card(card1, card2), card1)
 
     def test_can_cadd_cards_total(self):
         card1 = Card("Hearts", 2)
         card2 = Card("Hearts", 1)
-        self.assertEqual(
-            CardGame.cards_total(self, [card1, card2]), "You have a total of3"
-        )
+        cardGame = CardGame()
+        self.assertEqual(cardGame.cards_total([card1, card2]), "You have a total of3")
