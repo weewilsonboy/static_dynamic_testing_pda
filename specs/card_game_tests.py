4e33326a73d91d16df7abc0960a1e90bff703600
weewilsonboy <wilson.euan@gmail.com>
2023-11-06 12:33:26 +0000

    fixed scoping of all objects

--- specs/card_game_tests.py
+++ specs/card_game_tests.py
@@ -5,22 +5,17 @@ from src.card_game import CardGame
 
 class TestCardGame(unittest.TestCase):
     def setUp(self):
-        cardGame = CardGame()
+        self.cardGame = CardGame()
+        self.card1 = Card("Hearts", 2)
+        self.card2 = Card("Hearts", 1)
 
     def test_can_check_for_ace(self):
-        card1 = Card("Hearts", 2)
-        card2 = Card("Hearts", 1)
-        cardGame = CardGame()
-        self.assertEqual(cardGame.check_for_ace(card1), False)
+        self.assertEqual(self.cardGame.check_for_ace(self.card1), False)
 
     def test_can_find_highest_card(self):
-        card1 = Card("Hearts", 2)
-        card2 = Card("Hearts", 1)
-        cardGame = CardGame()
-        self.assertEqual(cardGame.highest_card(card1, card2), card1)
+        self.assertEqual(self.cardGame.highest_card(self.card1, self.card2), self.card1)
 
     def test_can_cadd_cards_total(self):
-        card1 = Card("Hearts", 2)
-        card2 = Card("Hearts", 1)
-        cardGame = CardGame()
-        self.assertEqual(cardGame.cards_total([card1, card2]), "You have a total of3")
+        self.assertEqual(
+            self.cardGame.cards_total([self.card1, self.card2]), "You have a total of3"
+        )
